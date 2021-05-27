cfdc1528505111b0be3a15ac7787c865aa3e654c
Natavar Prajapati <prajapatinatavar21197@gmail.com>
2021-05-27 14:40:19 +0530

    xvgfsrstr

--- app/src/main/java/com/example/retrofitandcoroutines/api/RetrofitBuilder.kt
+++ app/src/main/java/com/example/retrofitandcoroutines/api/RetrofitBuilder.kt
@@ -1,18 +1,13 @@
 package com.example.retrofitandcoroutines.api
 
+import com.example.retrofitandcoroutines.utils.Constants
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
 
-    const val BASE_URL = "https://newsapi.org/v2/"
-    const val API_KEY = "f0ff90e809ef4848885fad9b2250cd17"
-    const val SOURCES = "techcrunch"
-    const val CATEGORY = "business"
-    const val COUNTRY = "in"
-
     fun getInstance() = Retrofit.Builder()
-        .baseUrl(BASE_URL)
+        .baseUrl(Constants.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
--- app/src/main/java/com/example/retrofitandcoroutines/repository/NewsApiRepository.kt
+++ app/src/main/java/com/example/retrofitandcoroutines/repository/NewsApiRepository.kt
@@ -2,14 +2,15 @@ package com.example.retrofitandcoroutines.repository
 
 import androidx.annotation.WorkerThread
 import com.example.retrofitandcoroutines.api.RetrofitBuilder
+import com.example.retrofitandcoroutines.utils.Constants
 
 class NewsApiRepository() {
 
     @WorkerThread
     suspend fun getNewsTopHeadlines() = RetrofitBuilder.apiService
         .getNewsTopHeadlines(
-            RetrofitBuilder.SOURCES,
-            RetrofitBuilder.API_KEY
+            Constants.SOURCES,
+            Constants.API_KEY
         )
 
 }
