c17d96769893380c8af3eb3860a66c4ac6ff2982
Natavar Prajapati <prajapatinatavar21197@gmail.com>
2021-05-27 14:40:03 +0530

    xvgfsrstr

---
+++ app/src/main/java/com/example/retrofitandcoroutines/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.retrofitandcoroutines.utils
+
+object Constants {
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val API_KEY = "f0ff90e809ef4848885fad9b2250cd17"
+    const val SOURCES = "techcrunch"
+    const val CATEGORY = "business"
+    const val COUNTRY = "in"
+
+}
